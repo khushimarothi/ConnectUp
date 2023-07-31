b9056919bfac203923850510893d8dbaba1168e3
Khushi Marothi <khushimarothi2002@gmail.com>
2023-07-31 17:53:04 +0530

    My post section added

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 
 const HomePage = () => {
    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -22,7 +23,7 @@ const HomePage = () => {
         </Box>
         <Box flexBasis={isNonMobileScreens ? "42%" : undefined} 
         mt={isNonMobileScreens ? undefined : "2rem"}  >
-
+         <MyPostWidget picturePath={picturePath} />
           </Box>
           { isNonMobileScreens && (<Box flexBasis="26%" >  </Box>
           )}
